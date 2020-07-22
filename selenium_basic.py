679a1dfd75f6d57a0fd69754b466aaf41a167bdb
brainwashers <oliinyk.ihor@gmail.com>
2020-07-22 11:46:56 +0300

    selenium HW

--- selenium_basic.py
+++ selenium_basic.py
@@ -7,3 +7,15 @@
 # Click on [Normal success message button]
 
 # assert appeared green success message
+
+from selenium import webdriver
+import time
+
+driver = webdriver.Chrome()
+driver.get("https://www.seleniumeasy.com/test/bootstrap-alert-messages-demo.html")
+assert "Selenium" in driver.title
+elem = driver.find_element_by_id("normal-btn-success")
+elem.click()
+time.sleep(5)
+assert "normal success message" in driver.page_source
+driver.close()
